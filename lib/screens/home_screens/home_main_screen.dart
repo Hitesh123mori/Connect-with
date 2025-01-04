3362a6f9e4ef078cc790a585c905f854fdd3ecd9
harsh <harshmori0521@gmail.com>
2025-01-04 18:01:08 +0530

    des added in drawer

--- lib/screens/home_screens/home_main_screen.dart
+++ lib/screens/home_screens/home_main_screen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
     'Notifications',
     'Jobs',
   ];
-  void init(AppUserProvider appUserProvider)async{
+  void init(AppUserProvider appUserProvider) async {
     await appUserProvider.initUser();
   }
 
@@ -50,98 +50,133 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppUserProvider>(builder: (context,appUserProvider,child){
-      if(isFirst){
-        init(appUserProvider) ;
+    return Consumer<AppUserProvider>(
+        builder: (context, appUserProvider, child) {
+      if (isFirst) {
+        init(appUserProvider);
         isFirst = false;
       }
-      return  Scaffold(
+      return Scaffold(
         // backgroundColor: AppColors.theme['backgroundColor'],
         drawer: Drawer(
           backgroundColor: AppColors.theme['backgroundColor'],
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal:20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: SafeArea(
               child: Column(
                 children: [
                   InkWell(
-                    onTap: (){
-                      Navigator.push(context, LeftToRight(ProfileScreen())) ;
+                    onTap: () {
+                      Navigator.push(context, LeftToRight(ProfileScreen()));
                     },
                     child: Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10) ,
-                      ),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage: AssetImage("assets/other_images/photo.jpg"),
-                          ),
-                          SizedBox(width: 10,),
-                          Text(appUserProvider.user?.userName ?? "Name",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 16),),
-                        ],
-                      )
-                    ),
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage:
+                                  AssetImage("assets/other_images/photo.jpg"),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              appUserProvider.user?.userName ?? "Name",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ],
+                        )),
+                  ),
+                  Text(
+                    appUserProvider.user?.headLine ?? "Headline",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16),
                   ),
                   Divider(),
-                  DrawerContainer(data: appUserProvider.user?.followers.toString() ?? "0", label: 'Followers',) ,
-                  DrawerContainer(data: appUserProvider.user?.following.toString() ?? "0", label: 'Following',) ,
-                  DrawerContainer(data: appUserProvider.user?.searchCount.toString() ?? "0", label: 'Search Count',) ,
-                  DrawerContainer(data: appUserProvider.user?.profileViews.toString() ?? "0", label: 'Profile Views',) ,
-                  Expanded(child: Container()) ,
+                  DrawerContainer(
+                    data: appUserProvider.user?.followers.toString() ?? "0",
+                    label: 'Followers',
+                  ),
+                  DrawerContainer(
+                    data: appUserProvider.user?.following.toString() ?? "0",
+                    label: 'Following',
+                  ),
+                  DrawerContainer(
+                    data: appUserProvider.user?.searchCount.toString() ?? "0",
+                    label: 'Search Count',
+                  ),
+                  DrawerContainer(
+                    data: appUserProvider.user?.profileViews.toString() ?? "0",
+                    label: 'Profile Views',
+                  ),
+                  Expanded(child: Container()),
                   InkWell(
-                    onTap: ()async{
-                      await appUserProvider.logOut() ;
-                      HelperFunctions.showToast("Logout successfully") ;
-                      await Navigator.pushReplacement(context, RightToLeft(LoginScreen())) ;
+                    onTap: () async {
+                      await appUserProvider.logOut();
+                      HelperFunctions.showToast("Logout successfully");
+                      await Navigator.pushReplacement(
+                          context, RightToLeft(LoginScreen()));
                     },
                     child: Container(
                       height: 60,
                       decoration: BoxDecoration(
                         color: AppColors.theme['primaryColor'].withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(10) ,
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       child: Padding(
                         padding: const EdgeInsets.only(left: 20.0),
                         child: Row(
                           children: [
                             Icon(Icons.logout),
-                            SizedBox(width: 10,),
-                            Text("L O G O U T",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),)
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "L O G O U T",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            )
                           ],
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(height: 30,),
+                  SizedBox(
+                    height: 30,
+                  ),
                 ],
               ),
             ),
           ),
-
-
-
         ),
         appBar: AppBar(
           leading: Container(
             child: Builder(
-                builder: (context) =>InkWell(
-                  onTap:(){
-                    Scaffold.of(context).openDrawer();
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 5.0,top: 10,bottom: 10),
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage("assets/other_images/photo.jpg"),
-                      // backgroundColor: AppColors.theme['secondaryColor'].withOpacity(0.5),
-                      radius: 20,
-                      // child: Center(child: Text(appUserProvider.user?.userName?[0] ?? "U",style: TextStyle(color:
-                      // AppColors.theme['secondaryColor'],fontWeight: FontWeight.bold,fontSize: 20),)),
-                    ),
-                  ),
-                )
-            ),
+                builder: (context) => InkWell(
+                      onTap: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 5.0, top: 10, bottom: 10),
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage("assets/other_images/photo.jpg"),
+                          // backgroundColor: AppColors.theme['secondaryColor'].withOpacity(0.5),
+                          radius: 20,
+                          // child: Center(child: Text(appUserProvider.user?.userName?[0] ?? "U",style: TextStyle(color:
+                          // AppColors.theme['secondaryColor'],fontWeight: FontWeight.bold,fontSize: 20),)),
+                        ),
+                      ),
+                    )),
           ),
           backgroundColor: AppColors.theme['primaryColor'],
           centerTitle: true,
@@ -185,7 +220,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       );
-    }) ;
+    });
   }
 
   Widget _buildBody() {
@@ -198,4 +233,3 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 }
-
